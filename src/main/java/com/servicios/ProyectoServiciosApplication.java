161bc4f471c35cd6f0cee6695b12ff1627fd1246
kevin blas <cblas14@gmail.com>
2021-11-06 21:45:30 -0500

    primer commit

---
+++ src/main/java/com/servicios/ProyectoServiciosApplication.java
@@ -0,0 +1,13 @@
+package com.servicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoServiciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoServiciosApplication.class, args);
+	}
+
+}
